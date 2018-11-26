c24c8942e1c55cd96c56496bc66a9b0025ef08f4
Caleb Reed <cwreed@yahoo.com>
2018-11-26 14:55:06 -0600

    initial

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express'); // Server
+const bodyParser = require ('body-parser'); // JSON Middleware
+const logger = require('morgan'); // REST Logger
+const mongoose = require('mongoose'); // MongoDB ORM
+const routes = require("./routes");
+let db = require("./models"); // Require all models
+
+let PORT = process.env.PORT || 8080;
+let mongooseConnection = mongoose.connection;
+
+let app = express();
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true})); // Allows For JSON Interactions Between Client & Server
+app.use(express.static("client/build")); // Serve Static React Pages
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
+
+mongoose.Promise = global.Promise; // Set up promises with mongoose
+
+mongoose.connect( // Connect to the Mongo DB
+  process.env.MONGODB_URI || "mongodb://<dbuser>:<dbpassword>@ds143738.mlab.com:43738/heroku_137ccmcw"
+);
+
+mongooseConnection.on('error', console.error.bind(console, 'connection error:'));
+
+mongooseConnection.once('open', function() {
+  console.log(`Sucessfully Connected to Mongo DB !`); // If Connection is successful, Console.log(Message)
+});
+
+var cors = require("cors");
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(cors());
+
+app.post("/saveArticle", function(req, res) {
+   console.log("back path hit")
+   db.Article.create(req.body).then((lol)=>
+     console.log(lol)
+   );
+});
+
+
+app.use(routes); // Add routes, both API and View
+
+
+/////////////////////////////////////////////// /* Start Server */ //////////////////////////////////////////////////////////
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
